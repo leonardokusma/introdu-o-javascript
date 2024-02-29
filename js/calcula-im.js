7204b326804a943893c6cdd9cad932f122caf26b
leonardo kusma <leonardokusma48@gmail.com>
2024-02-28 21:40:09 -0300

    aula 2

---
+++ js/calcula-im.js
@@ -0,0 +1,23 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent
+
+
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var imc = calculaImc(peso, altura)
+
+    tdImc.textContent = imc;
+}
+function calculaImc(peso, altura ) {
+    var imc = 0;
+    imc = peso/ (altura**2);
+    return imc.toFixed(2);
+}
--- js/principal.js
+++ js/principal.js
@@ -1,23 +1,17 @@
 
 var titulo = document.querySelector(".titulo");
-console.log(titulo.textContent);
+
+
+/*
+
+//function titulo.addEventListener("click",function(){
+    console.log("Fui clicado!!!");
+});() {
+  //  console.log("Fui clicado!");
+//}onclick titulo
+
+*/
 titulo.textContent = "Aparecida Nutricionista ";
-//var paciente = document.querySelector("#primeiro-paciente");
-var pacientes = document.querySelectorAll(".paciente");
-
-for (var i = 0; i < pacientes.length; i++) {
-    var paciente = pacientes[i];
-
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
-
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent
 
 
-    var tdImc = paciente.querySelector(".info-imc");
-    var imc = peso / (altura ** 2)
-
-    tdImc.textContent = imc.toFixed(1);
-}
 
